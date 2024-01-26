9d9abeb2a6c0e3cec58c71535f094640a0b62a40
horatiu.dan <horatiu.dan@tangoe.com>
2024-01-26 15:58:43 +0200

    Initial commit.

---
+++ src/main/java/com/hcd/recordfilterstrategy/RecordFilterStrategyApplication.java
@@ -0,0 +1,13 @@
+package com.hcd.recordfilterstrategy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecordFilterStrategyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RecordFilterStrategyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hcd/recordfilterstrategy/RecordFilterStrategyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hcd.recordfilterstrategy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecordFilterStrategyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
